323c4077353d9af0b52474948eff748f3f9055ef
Cale MacDonald <cale.macdonald@gmail.com>
2021-08-21 11:26:15 -0600

    Adding copy to clipboard

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/calemacdonald/uuid
 
 go 1.16
 
-require github.com/google/uuid v1.3.0
+require (
+	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/google/uuid v1.3.0
+)
--- main.go
+++ main.go
@@ -1,16 +1,31 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"github.com/atotto/clipboard"
 	"github.com/google/uuid"
 	"os"
 )
 
 func main() {
+	copyToClipboard := flag.Bool("clip", false, "Copy to clipboard")
+	flag.Parse() // add this line
+
 	id, err := uuid.NewUUID()
 	if err != nil {
-		panic(err)
+		_, _ = fmt.Fprintf(os.Stderr, "Unable to generate UUID%s", LineBreak)
 	}
 
-	fmt.Fprintf(os.Stdout, "%s\n", id)
+	if *copyToClipboard == true {
+		err := clipboard.WriteAll(id.String())
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "Unable to copy to clipboard%s", LineBreak)
+		} else {
+			_, _ = fmt.Fprintf(os.Stdout, "Copied to clipboard%s", LineBreak)
+		}
+		return
+	}
+
+	_, _ = fmt.Fprintf(os.Stdout, "%s%s", id, LineBreak)
 }
